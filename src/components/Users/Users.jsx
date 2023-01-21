927d44de2888b32a705d1c1d74da5096f5c32670
Dzane <dzane@student.21-school.ru>
2023-01-21 21:05:49 +0700

    feat: class component Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,31 +1,44 @@
 import React from 'react'
 import style from './User/User.module.css'
+import axios from 'axios'
 
-const Users = (props) => {
-  return <div className={style.wrapper}>
-    {
-      props.users.map(u => <div className={style.container}>
-          <div className={style.subscribe}>
-            <div><img src={u.photo} alt="ava"/></div>
-            <div>
-              {
-                u.followed
-                  ? <button onClick={() => { props.unFollow(u.id) }}>UNFOLLOW</button>
-                  : <button onClick={() => { props.follow(u.id) }}>FOLLOW</button>
-              }
+class Users extends React.Component {
+  constructor (props) {
+    super(props)
+    axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+      this.props.setUsers(response.data.items)
+    })
+  }
+
+  render () {
+    return <div className={style.wrapper}>
+      {
+        this.props.users.map(u => <div key={u.id} className={style.container}>
+            <div className={style.subscribe}>
+              <div><img
+                src={u.photos.small != null ? u.small : 'https://avatars.mds.yandex.net/i?id=7406b5bf2010aace1c9c3d0381c45881f703f689-6250997-images-thumbs&n=13'}
+                alt="ava"/></div>
+              <div>
+                {
+                  u.followed
+                    ? <button onClick={() => { this.props.unFollow(u.id) }}>UNFOLLOW</button>
+                    : <button onClick={() => { this.props.follow(u.id) }}>FOLLOW</button>
+                }
+              </div>
+            </div>
+            <div className={style.about}>
+              <div>{u.name}</div>
+              <div>{u.status}</div>
+            </div>
+            <div className={style.about}>
+              <div>{'u.location.country'}</div>
+              <div>{'u.location.city'}</div>
             </div>
           </div>
-          <div className={style.about}>
-            <div>{u.fullName}</div>
-            <div>{u.status}</div>
-          </div>
-          <div className={style.about}>
-            <div>{u.location.country}</div>
-            <div>{u.location.city}</div>
-          </div>
-        </div>
-      )
-    }
-  </div>
+        )
+      }
+    </div>
+  }
 }
+
 export default Users
