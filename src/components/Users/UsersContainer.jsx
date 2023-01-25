2bc9697cc328315127bba84856dc9383d6160e32
Dzane <dzane@student.21-school.ru>
2023-01-25 12:46:15 +0700

    feat: refacted Users component and container

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,14 +1,43 @@
 import React from 'react'
-import { followAC, setUsersAC, unFollowAC } from '../../redux/userReducer'
+import { followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unFollowAC } from '../../redux/userReducer'
 import Users from './Users'
 import { connect } from 'react-redux'
+import axios from 'axios'
 
-const mapStateToProps = (state) => {
-  return {
-    users: state.userReducer.users
+
+class UsersContainer extends React.Component {
+  componentDidMount () {
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+      this.props.setUsers(response.data.items)
+      this.props.setTotalUsersCount(response.data.totalCount)
+    })
+  }
+
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber)
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+      this.props.setUsers(response.data.items)
+    })
+  }
+
+  render () {
+    return <Users totalUsersCount={this.props.totalUsersCount}
+                  pageSize={this.props.pageSize}
+                  currentPage={this.props.currentPage}
+                  follow={this.props.follow}
+                  unFollow={this.props.unFollow}
+                  onPageChanged={this.onPageChanged}
+                  users={this.props.users}/>
   }
 }
-
+const mapStateToProps = (state) => {
+  return {
+    users: state.userReducer.users,
+    pageSize: state.userReducer.pageSize,
+    totalUsersCount: state.userReducer.totalUsersCount,
+    currentPage: state.userReducer.currentPage
+  }
+}
 const mapDispatchToProps = (dispatch) => {
   return {
     follow: (userId) => {
@@ -19,10 +48,14 @@ const mapDispatchToProps = (dispatch) => {
     },
     setUsers: (users) => {
       dispatch(setUsersAC(users))
+    },
+    setCurrentPage: (page) => {
+      dispatch(setCurrentPageAC(page))
+    },
+    setTotalUsersCount: (totalCount) => {
+      dispatch(setTotalUsersCountAC(totalCount))
     }
   }
 }
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
-
-export default UsersContainer
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer)
--- src/redux/userReducer.js
+++ src/redux/userReducer.js
@@ -1,12 +1,19 @@
 const FOLLOW = 'FOLLOW'
 const UNFOLLOW = 'UNFOLLOW'
 const SET_USERS = 'SET_USERS'
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT'
 export const followAC = (userId) => ({ type: FOLLOW, userId })
 export const unFollowAC = (userId) => ({ type: UNFOLLOW, userId })
 export const setUsersAC = (users) => ({ type: SET_USERS, users })
+export const setCurrentPageAC = (page) => ({ type: SET_CURRENT_PAGE, page })
+export const setTotalUsersCountAC = (totalCount) => ({ type: SET_TOTAL_USERS_COUNT, totalCount })
 
 const initialState = {
-  users: []
+  users: [],
+  pageSize: 6,
+  totalUsersCount: 0,
+  currentPage: 1
 }
 
 const userReducer = (state = initialState, action) => {
@@ -32,6 +39,16 @@ const userReducer = (state = initialState, action) => {
     case SET_USERS:
       return {
         ...state, users: [...action.users]
+      }
+    case SET_CURRENT_PAGE:
+      return {
+        ...state,
+        currentPage: action.page
+      }
+    case SET_TOTAL_USERS_COUNT:
+      return {
+        ...state,
+        totalUsersCount: action.totalCount
       }
     default:
       return state
